c744ad0008b788e898fbc18bab76506d2ce2cf89
codewizrad <codewiizard@gmail.com>
2024-03-14 13:47:11 +0500

    s

--- src/components/leftProps.tsx
+++ src/components/leftProps.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 
-export default function Leftprops({img, text, color}){
+export default function Leftprops({img, text, color}: {img: StaticImageData; text:string; color:string}){
     return(
         <div className="cursor-pointer">
             <h1 className="text-[19px] flex items-center gap-3" style={{color: color}}><Image src={img} alt='image'/> {text}</h1>
